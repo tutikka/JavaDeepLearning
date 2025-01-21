178c96a06b6ffead5a7823f8e8dfbb9c1243218f
Tuomas Tikka <tuomas.tikka@bcbmedical.com>
2025-01-21 09:28:57 +0200

    Update Metrics.java

--- src/com/tt/javadeeplearning/metrics/Metrics.java
+++ src/com/tt/javadeeplearning/metrics/Metrics.java
@@ -3,14 +3,15 @@ package com.tt.javadeeplearning.metrics;
 public class Metrics {
 
     public static double accuracy(double[] truth, double[] predicted) {
-        int numCorrect = 0;
+        int correct = 0;
         for (int i = 0; i < truth.length; i++) {
-            if (predicted[i] == truth[i]) {
-                numCorrect++;
+            double t = truth[i];
+            double p = predicted[i];
+            if (p == t) {
+                correct++;
             }
         }
-        double a = (double) numCorrect / (double) truth.length;
-        return (a);
+        return ((double) correct / (double) truth.length);
     }
 
 }
